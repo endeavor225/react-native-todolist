5ca9a3beeac53dfce6ae3aee500d55f5cd843031
Abraham KONE <kone.abraham@jool-int.com>
2024-09-12 12:39:54 +0000

    Liste de tâche

--- App.style.js
+++ App.style.js
@@ -1,21 +1,16 @@
 import { StyleSheet } from "react-native";
 
 export const s = StyleSheet.create({
-    app:{
-        backgroundColor: "#F9F9F9", 
-        flex: 1,
-        padding: 10
-    },
-
-    header:{
-        flex: 1,
-    },
-
-    body:{
-        flex: 5,
-    },
-
-    footer:{
-        height: 70,
-    },
-  });
+  app: {
+    backgroundColor: "#F9F9F9",
+    flex: 1,
+    padding: 10,
+  },
+  cardItem: {
+    marginBottom: 20,
+    marginHorizontal: 3,
+  },
+  header: { flex: 1 },
+  body: { flex: 5 },
+  footer: { height: 70 },
+});
