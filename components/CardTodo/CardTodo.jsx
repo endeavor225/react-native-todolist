8c8ca596e1f95a9258258ebcc83325129d148b13
Abraham KONE <kone.abraham@jool-int.com>
2024-09-12 12:22:53 +0000

    Composant Card

---
+++ components/CardTodo/CardTodo.jsx
@@ -0,0 +1,19 @@
+import {  Image, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import checkImg from "../../assets/check.png"
+import { s } from './CardTodo.style'
+export default function CardTodo({ todo }) {
+  return (
+    <TouchableOpacity style={s.card}>
+      <Text
+        style={[
+          s.txt,
+          todo.isCompleted && { textDecorationLine: "line-through" },
+        ]}
+      >
+        {todo.title}
+      </Text>
+      {todo.isCompleted && <Image style={s.img} source={checkImg} />}
+    </TouchableOpacity>
+  )
+}
