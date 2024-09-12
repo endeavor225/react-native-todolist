7f9cf7ea1df691e58fc102980f10edfe5360cd65
Abraham KONE <kone.abraham@jool-int.com>
2024-09-12 12:02:43 +0000

    Composant Header

--- App.style.js
+++ App.style.js
@@ -3,7 +3,8 @@ import { StyleSheet } from "react-native";
 export const s = StyleSheet.create({
     app:{
         backgroundColor: "#F9F9F9", 
-        flex: 1
+        flex: 1,
+        padding: 10
     },
 
     header:{
